1dc5680d6c5cac5d6e727def28dcecf9348fc850
GuiQRSM <endbeob15@gmail.com>
2022-02-10 15:11:13 -0300

    Configure the 'Scaffold()' class with the 'body: ,' attribute

--- lib/ActionMain_Frame.dart
+++ lib/ActionMain_Frame.dart
@@ -9,6 +9,7 @@ class ActionMain extends StatefulWidget {
 
 class _ActionMainState extends State<ActionMain> {
 
+  List _itemList = ["Item: 1", "Item: 2", "Item: 3", "Item: 4", "Item: 5"];
   var mainColor = Color.fromRGBO(92, 138, 138, 1);
   var secColor = Color.fromRGBO(159, 223, 190, 1);
 
@@ -24,6 +25,33 @@ class _ActionMainState extends State<ActionMain> {
           color: secColor
         ),
         backgroundColor: mainColor,
+      ),
+      body: Container(
+        width: double.infinity,
+        padding: EdgeInsets.all(16),
+        child: Column(
+          children: <Widget>[
+            Expanded(
+                child: ListView.builder(
+                    itemCount: _itemList.length,
+                    itemBuilder: (context, index){
+
+                      return ListTile(
+                        title: Text(
+                          _itemList[index],
+                          style: TextStyle(
+                            fontSize: 22,
+                            fontWeight: FontWeight.w600,
+                            color: mainColor
+                          ),
+                        ),
+                      );
+
+                    },
+                ),
+            ),
+          ],
+        ),
       ),
     );
   }
