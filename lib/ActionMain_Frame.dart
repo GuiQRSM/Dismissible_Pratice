51252ddf5458b2608b855f1f8181ffff7624101c
GuiQRSM <endbeob15@gmail.com>
2022-02-10 14:56:21 -0300

    Implement in the 'lib' folder the dart file 'ActionMain_Frame'

---
+++ lib/ActionMain_Frame.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ActionMain extends StatefulWidget {
+  const ActionMain({Key? key}) : super(key: key);
+
+  @override
+  _ActionMainState createState() => _ActionMainState();
+}
+
+class _ActionMainState extends State<ActionMain> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
